b090bf43b36838e0f1e7606b37d5f9fd2f3a6cc8
evolgen144 <phillouis@gmail.com>
2023-03-18 07:18:06 -0400

    Login Test  Not Yet Functional

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,27 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+/* Auth0 */
+import { Auth0Provider } from '@auth0/auth0-react';
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const id = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
+
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Auth0Provider
+      domain="dev-dgee66u0thfy5ho2.us.auth0.com"
+      clientId="VCpD6fxumwLhWCtSRzxMsVispXMQReTQ"
+      useRefreshTokens={true}
+      useRefreshTokensFallback={false}
+      authorizationParams={{ redirect_uri:window.location.origin}}
+      // authorizationParams={{redirect_uri:"http://localhost:8100/Hire"}}
+    >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Auth0Provider>
 );
 
  
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,47 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { useHistory } from "react-router-dom";
 import {
-    IonContent,
-    IonPage,
-    IonInput,
-    IonButton,
-    IonLabel,
-    IonItem,
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonButton,
     } from "@ionic/react";
 
-type FormData = {
-    email: string;
-    username: string;
-    };
+/* Login imports */
+import LoginButton from '../components/LoginButton'
+
 
 const LoginPage: React.FC = () => {
-    const { register, handleSubmit } = useForm<FormData>();
-    const history = useHistory();
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const history = useHistory();
     
-    const onSubmit = (data: FormData) => {
-        console.log("Login data:", data);
+    const handleLogin = () => {
+        console.log('Email:', email);
+        console.log('Password:', password);
         history.push('/Hire')
       };
 
     return (
-        <IonPage>
-          <IonContent>
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <IonItem>
-                <IonLabel>Email:</IonLabel>
-                <IonInput type="email" {...register("email")} />
-              </IonItem>
-              <IonItem>
-                <IonLabel>Username:</IonLabel>
-                <IonInput type="text" {...register("username")} />
-              </IonItem>
-              <IonButton expand="block" type="submit">
-                Login
-              </IonButton>
-            </form>
-          </IonContent>
-        </IonPage>
+      <LoginButton />
+
+
+
+    //   <IonPage>
+    //   <IonHeader>
+    //     <IonToolbar>
+    //       <IonTitle>Login</IonTitle>
+    //     </IonToolbar>
+    //   </IonHeader>
+    //   <IonContent>
+    //     <IonGrid>
+    //       <IonRow>
+    //         <IonCol>
+    //           <IonItem>
+    //             <IonLabel position="floating">Email</IonLabel>
+    //             <IonInput
+    //               type="email"
+    //               value={email}
+    //               onIonChange={(e) => setEmail(e.detail.value!)}
+    //             />
+    //           </IonItem>
+    //         </IonCol>
+    //       </IonRow>
+    //       <IonRow>
+    //         <IonCol>
+    //           <IonItem>
+    //             <IonLabel position="floating">Password</IonLabel>
+    //             <IonInput
+    //               type="password"
+    //               value={password}
+    //               onIonChange={(e) => setPassword(e.detail.value!)}
+    //             />
+    //           </IonItem>
+    //         </IonCol>
+    //       </IonRow>
+    //       <IonRow>
+    //         <IonCol>
+    //           <IonButton expand="block" onClick={handleLogin}>
+    //             Login
+    //           </IonButton>
+    //         </IonCol>
+    //       </IonRow>
+    //     </IonGrid>
+    //   </IonContent>
+    // </IonPage>
       );
     };
 
