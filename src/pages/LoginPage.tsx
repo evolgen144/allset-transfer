01e9c85e91d2c18d13d20d1006f58a40bfd6b245
evolgen144 <phillouis@gmail.com>
2023-03-19 16:28:27 -0400

    Login and new account done

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -41,53 +41,64 @@ const LoginPage: React.FC = () => {
   return (
     <IonPage>
       <IonContent className="ion-padding">
-        <IonGrid className="as_grid">
-              <IonRow>
-                  <IonCol size="12" className="ion-text-center colSize">
-                    <div className="container">
-                    <IonImg src={logo} alt="AllSetLogo" style={{ height: '20%' }}/>
-                      <IonText>
-                        <p>
-                          Welcome! Please take a moment to read our{' '}
-                          <a
-                            href="#"
-                            onClick={(e) => {
-                              e.preventDefault();
-                              setShowTerms(true);
-                            }}
-                          >
-                            terms and conditions
-                          </a>
-                          .
-                        </p>
-                      </IonText>
-
-                      <IonRadioGroup value={agreed} onIonChange={(e) => setAgreed(e.detail.value)}>
-                        <IonItem lines="none">
-                          <IonRadio value={true} />
-                          <IonLabel>I agree</IonLabel>
-                        </IonItem>
-                      </IonRadioGroup>
-
-                      <IonButton expand="full" color={agreed ? 'allset' : 'allset'} onClick={() => loginWithRedirect()} disabled={!agreed}>
-                        Continue
-                      </IonButton>
-                    </div>
-
-                    <IonModal isOpen={showTerms} onDidDismiss={() => setShowTerms(false)}>
-                      <IonContent className="ion-padding">
-                        <h2>Terms and Conditions</h2>
-                        <p>Put your terms and conditions content here.</p>
-                        <IonButton expand="full" color="light" onClick={() => setShowTerms(false)}>
-                          Close
-                        </IonButton>
-                      </IonContent>
-                    </IonModal>
+        <IonGrid className="ion-grid-center">
+          <IonRow className="ion-text-center">
+              <div className="container">
+                <IonRow>
+                  <IonCol size="12" className="ion-text-center">
+                    <IonImg src={logo} alt="AllSetLogo" style={{ height: '50px' }} />
                   </IonCol>
                 </IonRow>
-          </IonGrid>
-      </IonContent>
-    </IonPage>
+                <IonRow>
+                  <IonText>
+                    <p className="intro_text">
+                    Collaborate with confidence
+                    </p>
+                    <p className="intro_text2">
+                    AllSet is the go-to app for content production professionals to find and connect with the best talent in the industry.
+                    </p>
+                  </IonText>
+                </IonRow>
+                <IonRow className="ion-text-center">
+                  <IonText>
+                    <p>
+                      Please take a moment to read our{' '}
+                      <a
+                        href="#"
+                        onClick={(e) => {
+                          e.preventDefault();
+                          setShowTerms(true);
+                        }}
+                      >
+                        terms and conditions
+                      </a>
+                    </p>
+                  </IonText>
+                </IonRow>
+                <IonRadioGroup allow-empty-selection="true" value={agreed} onIonChange={(e) => setAgreed(e.detail.value)}>
+                  <IonItem lines="none">
+                    <IonRadio value={true} />
+                    <IonLabel>&nbsp;&nbsp;&nbsp;I agree</IonLabel>
+                  </IonItem>
+                </IonRadioGroup>
+                <IonButton expand="full" color={agreed ? 'allset' : 'allset'} onClick={() => loginWithRedirect()} disabled={!agreed}>
+                  Continue
+                </IonButton>
+              </div>
+
+              <IonModal isOpen={showTerms} onDidDismiss={() => setShowTerms(false)}>
+                <IonContent className="ion-padding">
+                    <h2>Terms and Conditions</h2>
+                    <p>Put your terms and conditions content here.</p>
+                  <IonButton expand="full" color="light" onClick={() => setShowTerms(false)}>
+                    Close
+                  </IonButton>
+                </IonContent>
+              </IonModal>
+          </IonRow>
+        </IonGrid>
+    </IonContent>
+  </IonPage>
   );
 };
 
